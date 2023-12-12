3a26832fcefec9480097462850cc905d7b8d51a1
jomel argoncillo <jomel.argoncillo@hcdc.edu.ph>
2023-12-12 18:08:10 +0800

    commit (2)

--- webDev_Finals_Argoncillo/js/darkmode.js
+++ webDev_Finals_Argoncillo/js/darkmode.js
@@ -1,3 +1,17 @@
+function changeText() {
+    // Get the button element by its ID
+    var button = document.getElementById("dmButton");
+
+    // Check the current text of the button
+    if (button.innerHTML === "🌙" && localStorage.getItem('dark-mode') === 'enabled') {
+        // Change the text if it's the original text
+        button.innerHTML = "☀️";
+    } if (button.innerHTML === "🌙" && localStorage.getItem('dark-mode') === 'disabled'){
+        // Change the text back to the original if it's already changed
+        button.innerHTML = "☀️";
+    } else {button.innerHTML = "🌙";}
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     const darkModeToggle = document.getElementById('dark-mode-checkbox');
     const body = document.body;
